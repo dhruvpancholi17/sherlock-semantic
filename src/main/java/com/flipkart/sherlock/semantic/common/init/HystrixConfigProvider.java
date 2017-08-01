cc4b7f65d0538489b720aca16759ad40ad1be391
Anurag Laddha <anurag.laddha@flipkart.com>
2017-08-01 17:58:25 +0530

    DS-1287: Exposing hystrix metrics using codahale metric registry

--- src/main/java/com/flipkart/sherlock/semantic/common/init/HystrixConfigProvider.java
+++ src/main/java/com/flipkart/sherlock/semantic/common/init/HystrixConfigProvider.java
@@ -3,9 +3,12 @@ package com.flipkart.sherlock.semantic.common.init;
 import com.flipkart.sherlock.semantic.common.config.Constants;
 import com.flipkart.sherlock.semantic.common.hystrix.ConfigServiceHystrixConfigFetcher;
 import com.flipkart.sherlock.semantic.common.hystrix.IHystrixConfigFetcher;
+import com.flipkart.sherlock.semantic.common.util.JmxMetricRegistry;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.google.inject.name.Names;
+import com.netflix.hystrix.contrib.codahalemetricspublisher.HystrixCodaHaleMetricsPublisher;
+import com.netflix.hystrix.strategy.HystrixPlugins;
 
 /**
  * Created by anurag.laddha on 27/07/17.
@@ -26,5 +29,9 @@ public class HystrixConfigProvider extends AbstractModule {
             .toInstance(this.hystrixConfigCacheRefreshTimeSec);
 
         bind(IHystrixConfigFetcher.class).to(ConfigServiceHystrixConfigFetcher.class).in(Singleton.class);
+
+        //Exposes hystrix metrics using given metric registry.
+        HystrixPlugins.getInstance().registerMetricsPublisher(new HystrixCodaHaleMetricsPublisher(
+            JmxMetricRegistry.INSTANCE.getInstance()));
     }
 }
