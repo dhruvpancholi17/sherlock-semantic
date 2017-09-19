0814fd7479514c71c4e37d01d13a1ddbc3a74b16
Anurag Laddha <anurag.laddha@flipkart.com>
2017-09-19 20:53:42 +0530

    DS-1358: search for urls based on store only in last 2 days

--- src/main/scripts/cacherefresh/semanticservice/store/semanticServiceCacheRefreshStoreUnix.sh
+++ src/main/scripts/cacherefresh/semanticservice/store/semanticServiceCacheRefreshStoreUnix.sh
@@ -11,8 +11,8 @@ USERNAME=monish.gandhi
 PORT=25280
 #Fetch ips for semantic service from iaas API
 HOSTS=`curl -s "http://10.33.65.0:8080/compute/v1/apps/sherlock-app-cloud/instances?view=summary" | tr "," "\n" | grep -B6 "semantic-service" | grep primary | cut -d '"' -f4  | tr "\n" " ")`
-CURRENT_DATE=$(date '+%d/%h/%Y' | tr [:lower] [:upper])
-PREVIOUS_DATE=$(date --date="yesterday" '+%d/%h/%Y' | tr [:lower] [:upper])
+CURRENT_DATE=$(date '+%Y-%m-%d')
+PREVIOUS_DATE=$(date --date="yesterday" '+%Y-%m-%d')
 
 echo "Aggregating logs for $PREVIOUS_DATE and $CURRENT_DATE in $FILE"
 for QUERY in $(echo $QUERIES | sed "s/,/ /g") ; do
@@ -29,7 +29,7 @@ for QUERY in $(echo $QUERIES | sed "s/,/ /g") ; do
     echo "Escaped store path: $QUERY";
 
     for HOSTNAME in ${HOSTS} ; do
-      ssh -i keymo -oStrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "grep -e "$CURRENT_DATE" -e "$PREVIOUS_DATE" /var/log/flipkart/w3/sherlock/access.2017-*.log | grep '/semantic' | grep '$QUERY'|  cut -d' ' -f7" >> $FILE
+      ssh -i keymo -oStrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "grep '/semantic' /var/log/flipkart/w3/sherlock/access.$CURRENT_DATE.log /var/log/flipkart/w3/sherlock/access.$PREVIOUS_DATE.log | grep '$QUERY'| cut -d' ' -f7" >> $FILE
     done
 
 done
